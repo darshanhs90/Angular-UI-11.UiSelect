6a471ade391123606b9503a9abf204275c8f5923
darshanhs90 <hsdars@gmail.com>
2015-04-05 14:16:51 +0530

    Ver 1.0

---
+++ app.js
@@ -0,0 +1,17 @@
+var app=angular.module("myApp",['ui.select']);
+app.controller("MainController",function($scope){
+$scope.employee={};
+$scope.employees=[{name:'asdasd',email:'dasdas@asd.com'},
+                  {name:'asdasd1',email:'dasdas1@asd.com'},
+                  {name:'asdasd2',email:'dasdas2@asd.com'},
+                  {name:'asdasd3',email:'dasdas3@asd.com'},
+                  {name:'asdasd4',email:'dasdas4@asd.com'},
+                  {name:'asdasd5',email:'dasdas5@asd.com'},
+                  {name:'asdasd6',email:'dasdas6@asd.com'},
+                  {name:'asdasd7',email:'dasdas7@asd.com'},
+                  {name:'asdasd8',email:'dasdas8@asd.com'},
+                  {name:'asdasd9',email:'dasdas9@asd.com'},
+                  {name:'asdasd0',email:'dasdas0@asd.com'}
+                  ];
+
+});
